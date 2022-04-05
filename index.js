d9d63cf1b5919b261c0f480e941409eeea343848
choisukjune <sukjune.choi@gmail.com>
2022-04-05 13:11:14 +0900

    .

--- index.js
+++ index.js
@@ -55,6 +55,138 @@ global.CONST.SERVER.API ={}
 global.CONST.SERVER.API.AUTOH = { protocol: "http://", host : "localhost", port : "8889" } //오토핫키 크롤링서버
 
 
+//-------------------------------------------------------;
+// VARIABLE;
+//-------------------------------------------------------;
+
+var ROOT_PATH = process.cwd();
+
+var CP_COMMAND = {};
+	CP_COMMAND.MONGO = "..\\Binary\\Mongodb\\mongodb-win32-x86_64-windows-4.4.3\\bin\\mongo";
+	//CP_COMMAND.MONGO = "mongo";
+
+var DBJS_DIRECTORY_PATH = ROOT_PATH + "/dbjs/";
+var _tDbjs_PATH = ROOT_PATH + "/tdbjs/";
+
+//-------------------------------------------------------;
+// FUNCTION;
+//-------------------------------------------------------;
+//-------------------------;
+//-------------------------;
+//-------------------------;
+//-------------------------;
+//-------------------------;
+
+/*
+ * @function
+ * @param {String} dbjsNm
+ * @param {boolean} bResult
+ * @return {String} r
+ */
+var exec_query_DB = function( dbjsNm, bResult ){
+
+	var DBJS_NM = dbjsNm;
+	var FILE_PATH = ROOT_PATH + "/dbjs/" + DBJS_NM;
+	var fileNm = ROOT_PATH + "\\result\\" + dbjsNm + "__" + Date.now() 
+	
+	var _t_command = CP_COMMAND.MONGO + " --username <!=ID=!> --password <!=PWD=!> --authenticationDatabase admin --host <!=HOST=!> --port <!=PORT=!> admin \"<!=FILE_PATH=!>\"";
+	    _t_command = _t_command + " > " + fileNm + ".result";
+	
+	var command = _t_command.replace( "<!=ID=!>", global.CONST.MongoDB.OPTIONS.self.ID )
+		.replace( "<!=PWD=!>", global.CONST.MongoDB.OPTIONS.self.PWD )
+		.replace( "<!=HOST=!>", global.CONST.MongoDB.OPTIONS.self.HOST )
+		.replace( "<!=PORT=!>", global.CONST.MongoDB.OPTIONS.self.PORT )
+		.replace( "<!=FILE_PATH=!>", FILE_PATH );
+	//console.log( command )
+	execSync( command );
+	var r = fs.readFileSync( fileNm + ".result" ).toString();
+		r = deleteLines( r , 4 )
+	return r;
+};
+
+//-------------------------;
+//-------------------------;
+//-------------------------;
+//-------------------------;
+//-------------------------;
+/*
+ * @function
+ * @param {String} str
+ * @param {Number} n
+ * @return {String} str
+ */
+var deleteLines = function( str, n ){
+	var i = 0,iLen = n,io;
+	for(;i<iLen;++i){ str = str.slice(str.indexOf("\n") + 1, str.length ); }
+	//str = str.replace( /\t/g, '' );
+	//str = str.replace( /\r\n/g, '' );
+	return str;
+};
+//-------------------------;
+//-------------------------;
+//-------------------------;
+//-------------------------;
+//-------------------------;
+/*
+ * @function
+ * @param {String} url
+ * @return {Object} o
+ */
+var paramToObject = function( _url ){
+	
+//	var r =  url.split("?")[ 1 ];
+//	var a = r.split("&");
+//	var o = {};
+//	var i = 0,iLen = a.length,io;
+//	
+//	for(;i<iLen;++i){
+//		io = a[ i ];
+//		var _ta = io.split( "=" );
+//		o[ _ta[0] ] = _ta[ 1 ];
+//	}
+//	console.log( o )
+	var queryData = url.parse( _url, true).query;
+	return queryData;
+};
+
+var numberWithCommas = function(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
+var dateFormat_YYMMDDHHMMSS = function( date ){
+	date = date || new Date();
+
+	var YYYY = date.getFullYear();
+	var MM = pad( date.getMonth() + 1, 2 );
+	var DD = pad( date.getDate(), 2 );
+	var H = pad( date.getHours(), 2 );
+	var M = pad( date.getMinutes(), 2 );
+	var S = pad( date.getSeconds(), 2 );
+
+	return YYYY + MM + DD + H +  M + S;
+};
+
+var dateFormat_YYMMDD = function( date ){
+	date = date || new Date();
+
+	var YYYY = date.getFullYear();
+	var MM = pad( date.getMonth() + 1, 2 );
+	var DD = pad( date.getDate(), 2 );
+//	var H = pad( date.getHours(), 2 );
+//	var M = pad( date.getMinutes(), 2 );
+//	var S = pad( date.getSeconds(), 2 );
+
+	return YYYY + MM + DD;// + H +  M + S;
+};
+
+
+
+var pad = function(n, width){
+	n = n + '';
+	return n.length >= width ? n : new Array(width - n.length + 1).join('0') + n;
+}
+
+
 //-------------------------;
 //-------------------------;
 //-------------------------;
@@ -696,6 +828,7 @@ global.wsFuns.renderTradeValueInfo_gap = function(){
 		p : null
 	}
 	global.ws.boradCastMessage( r )
+	global.wsFns.insertTradeValueInfo_gap( r.d )
 	console.log( "[ E ] - global.wsFuns.renderTradeValueInfo_gap" )
 }
 
@@ -763,6 +896,34 @@ global.ws.clearIntervals.renderTradeValueInfo_gap 	= function(){ clearInterval( 
 //--------------------------------------------------;
 //--------------------------------------------------;
 //--------------------------------------------------;
+
+
+global.wsFns.insertTradeValueInfo_gap = function( d ){
+	console.log( "[ S ] - global.wsFns.insertTradeValueInfo_gap" )
+	var _tdbjs_nm = "insertTradeValueInfo_gap";
+	console.log( _tDbjs_PATH + "/" + _tdbjs_nm + ".tdbjs" ); 
+	try
+	{
+		var _tQuery = fs.readFileSync( _tDbjs_PATH + "/" + _tdbjs_nm + ".tdbjs" ).toString();
+	}
+	catch( err )
+	{
+		console.log( routerNm + " - DBJS File Not Found! - " + err );
+		res.end("{ sucess : 0, data : null }");
+	}
+	var query = _tQuery
+				.replace( "<!=DATA=!>", d )
+				.replace( "<!=DATE=!>", dateFormat_YYMMDD() );
+
+	var dbjs_nm = "insertTradeValueInfo_gap.dbjs";
+
+	var FILE_PATH = DBJS_DIRECTORY_PATH + dbjs_nm;
+	fs.writeFileSync( DBJS_DIRECTORY_PATH + dbjs_nm , query, { flag : "w" } );
+	var r = exec_query_DB( dbjs_nm )
+	
+	console.log( "[ E ] - global.wsFns.insertTradeValueInfo_gap" )
+}
+
 //--------------------------------------------------;
 //--------------------------------------------------;
 //--------------------------------------------------;
