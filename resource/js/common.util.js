387a95996bd3edbc984ba8227e12fdf0a2b6182a
choisukjune <sukjune.choi@gmail.com>
2022-04-08 12:29:22 +0900

    .

--- resource/js/common.util.js
+++ resource/js/common.util.js
@@ -1,5 +1,128 @@
 window.UTIL = {}
-
-var numberWithCommas = function(x) {
+window.UTIL.Number = {}
+/*
+ *
+ */
+window.UTIL.Number.numberWithCommas = function(x) {
     return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
+
+window.UTIL.Date = {}
+
+/*
+ *
+ */
+window.UTIL.Date.getTimeTo__HHMMSS = function(date){
+	
+	date = date || new Date();
+	//var year = date.getFullYear();
+	//var month = date.getMonth() + 1;
+	//var day = date.getDate();
+	var hour = date.getHours();
+	var minute = date.getMinutes();
+	var second = date.getSeconds();
+
+	//month = month >= 10 ? month : '0' + month;
+	//day = day >= 10 ? day : '0' + day;
+	hour = hour >= 10 ? hour : '0' + hour;
+	minute = minute >= 10 ? minute : '0' + minute;
+	second = second >= 10 ? second : '0' + second;
+	var r = ( hour + minute + second ) * 1;
+
+	return r
+
+}
+/*
+ *
+ */
+window.UTIL.Date.getTimeTo__YYYYMMDD = function(date){
+	
+	date = date || new Date();
+	var year = date.getFullYear();
+	var month = date.getMonth() + 1;
+	var day = date.getDate();
+//	var hour = date.getHours();
+//	var minute = date.getMinutes();
+//	var second = date.getSeconds();
+
+	month = month >= 10 ? month : '0' + month;
+	day = day >= 10 ? day : '0' + day;
+	//hour = hour >= 10 ? hour : '0' + hour;
+	//minute = minute >= 10 ? minute : '0' + minute;
+	//second = second >= 10 ? second : '0' + second;
+	var r = year + month + day
+
+	return r
+
+}
+/*
+ *
+ */
+window.UTIL.Date.getTimeTo__YYYYMMDD_HHMMSS = function(date){
+	
+	date = date || new Date();
+	var year = date.getFullYear();
+	var month = date.getMonth() + 1;
+	var day = date.getDate();
+	var hour = date.getHours();
+	var minute = date.getMinutes();
+	var second = date.getSeconds();
+
+	month = month >= 10 ? month : '0' + month;
+	day = day >= 10 ? day : '0' + day;
+	hour = hour >= 10 ? hour : '0' + hour;
+	minute = minute >= 10 ? minute : '0' + minute;
+	second = second >= 10 ? second : '0' + second;
+	var r = year + "-" + month + "-" + day + " " + hour + ":" + minute+ ":" + second
+
+	return r
+
+}
+/*
+ *
+ */
+window.UTIL.Date.getTimeTo__HHMMSS = function(date){
+	
+	date = date || new Date();
+	//var year = date.getFullYear();
+	//var month = date.getMonth() + 1;
+	//var day = date.getDate();
+	var hour = date.getHours();
+	var minute = date.getMinutes();
+	var second = date.getSeconds();
+
+	//month = month >= 10 ? month : '0' + month;
+	//day = day >= 10 ? day : '0' + day;
+	hour = hour >= 10 ? hour : '0' + hour;
+	minute = minute >= 10 ? minute : '0' + minute;
+	second = second >= 10 ? second : '0' + second;
+	//var r = year + "-" + month + "-" + day + " " + hour + ":" + minute+ ":" + second
+	var r =  hour.toString() + minute.toString() + second.toString()
+	return r*1
+
+}
+
+/*
+ *
+ */
+window.UTIL.Date.getTimeTo__YYYY_MM_DD = function(date){
+	
+	date = date || new Date();
+	var year = date.getFullYear();
+	var month = date.getMonth() + 1;
+	var day = date.getDate();
+//	var hour = date.getHours();
+//	var minute = date.getMinutes();
+//	var second = date.getSeconds();
+
+	month = month >= 10 ? month : '0' + month;
+	day = day >= 10 ? day : '0' + day;
+	//hour = hour >= 10 ? hour : '0' + hour;
+	//minute = minute >= 10 ? minute : '0' + minute;
+	//second = second >= 10 ? second : '0' + second;
+	var r = year + "-" + month + "-" + day
+
+	return r
+
 }
