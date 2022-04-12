da7642b979d639975ce6bc461d4b04e3bf9c6800
choisukjune <sukjune.choi@gmail.com>
2022-04-12 18:33:13 +0900

    .

--- resource/js/common.util.js
+++ resource/js/common.util.js
@@ -165,6 +165,30 @@ window.UTIL.Date.getTimeTo__YYYY_MM_DD = function(date){
 
 }
 
+/*
+ *
+ */
+window.UTIL.Date.getTimeTo__YYYYMMDD_berfore_day = function(date,day){
+	
+	date = date || new Date();
+	date.setDate( date.getDate() - day );
+	var year = date.getFullYear();
+	var month = date.getMonth() + 1;
+	var day = date.getDate();
+//	var hour = date.getHours();
+//	var minute = date.getMinutes();
+//	var second = date.getSeconds();
+
+	month = month >= 10 ? month : '0' + month;
+	day = day >= 10 ? day : '0' + day;
+	//hour = hour >= 10 ? hour : '0' + hour;
+	//minute = minute >= 10 ? minute : '0' + minute;
+	//second = second >= 10 ? second : '0' + second;
+	var r = year.toString() + month.toString() + day.toString()
+
+	return r
+
+}
 
 
 window.UTIL.Color = {};
@@ -235,3 +259,41 @@ window.UTIL.Html.htmlToElement = function( html ){
     template.innerHTML = html;
     return template.content.firstChild;
 }
+
+window.COMPONENT = {};
+window.COMPONENT.gnb = function(){
+	
+	var today = window.UTIL.Date.getTimeTo__YYYYMMDD()
+	var yesterday = window.UTIL.Date.getTimeTo__YYYYMMDD_berfore_day( null, 1 );
+
+	var _target_el = window.document.getElementById( "gnb" );
+
+	var html = `
+		<span id="menu_00" style="cursor:pointer;">실시간대량거래</span> | 
+		<span id="menu_01" style="cursor:pointer;">기관거래원별</span> | 
+		<span id="menu_02" style="cursor:pointer;">외국계거래원별</span> | 
+		<span id="menu_03" style="cursor:pointer;">외국계증권사매매동향</span> | 
+		<span id="menu_04" style="cursor:pointer;">wics업종별현황</span> |
+		<span id="menu_05" style="cursor:pointer;">관심종목</span>
+	`
+	_target_el.innerHTML = html;
+
+	var urls = {
+		menu_00 : `/html/실시간대량거래.html`,
+		menu_01 : `/html/기관거래원별.html?date=${today}`,
+		menu_02 : `/html/외국계거래원별.html?date=${today}`,
+		menu_03 : `/html/외국계증권사매매동향.html?date=${today}`,
+		menu_04 : `/html/wics업종별현황.html?date=${today}`,
+		menu_05 : `/html/관심종목.html?date=${yesterday}`,
+		candle : `/html/candle.html?cd=`
+	};
+
+	window.document.getElementById( "menu_00 ").addEventListener('click',function(e){ window.UTIL.Link.a(urls.menu_00,"_blank"); });
+	window.document.getElementById( "menu_01 ").addEventListener('click',function(e){ window.UTIL.Link.a(urls.menu_01,"_blank"); });
+	window.document.getElementById( "menu_02 ").addEventListener('click',function(e){ window.UTIL.Link.a(urls.menu_02,"_blank"); });
+	window.document.getElementById( "menu_03 ").addEventListener('click',function(e){ window.UTIL.Link.a(urls.menu_03,"_blank"); });
+	window.document.getElementById( "menu_04 ").addEventListener('click',function(e){ window.UTIL.Link.a(urls.menu_04,"_blank"); });
+	window.document.getElementById( "menu_05 ").addEventListener('click',function(e){ window.UTIL.Link.a(urls.menu_05,"_blank"); });
+	
+	return;
+};
