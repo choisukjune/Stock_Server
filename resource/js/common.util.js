c0eed99103b9109f8bae77286bb07d910cfcc6b7
choisukjune <sukjune.choi@gmail.com>
2022-04-14 13:10:58 +0900

    Update common.util.js

--- resource/js/common.util.js
+++ resource/js/common.util.js
@@ -1406,17 +1406,16 @@ window.COMPONENT.renderStockInfo = function( cd ){
 	var _htmlTxt = `
 	<div class="sixteen wide column" style="">
 		<div class="ui grid">
-			<div class="sixteen wide column" style="">
-				<span style='font-size:12px;padding:10px 0px;color:#ccc;'>${ d.date }</span>
-				<span style='font-size:15px;padding:10px 0px;'>${ d.name }</span><br>
+			<div class="ten wide column" style="">
+				<span style='font-size:12px;padding:10px 0px;color:#ccc;'>${ d.date }</span><br>
+				<span style='font-size:15px;padding:10px 0px;'>${ d.name }</span>
 				<span style='font-size:12px;padding:10px 0px;'>( ${ d.symbolCode } ) - ${d.market} </span><br>
 				<span style="color:${color};font-size:18px;font-weight:bold;">  ${window.UTIL.Number.numberWithCommas( d.tradePrice )}</span> 
 				<span style="color:${color};"> ${symbol} ${window.UTIL.Number.numberWithCommas( d.changePrice )} ( ${(d.changeRate*100).toFixed(2)} % )</span><br>
 				<span style="font-size:12px;">  거래량 : ${window.UTIL.Number.numberWithCommas( d.accTradeVolume )}( ${tradeVolumeRatio}% ) | 거래대금 ${window.UTIL.Number.numberWithCommas( d.accTradePrice )} </span>  
 			</div>
-			<div class="sixteen wide column" style="">
+			<div class="six wide column" style="">
 				<div claa="image"><img src="https://t1.daumcdn.net/finance/chart/kr/stock/d/A${cd}.png?t=${Date.now()}"  style="width:-webkit-fill-available;"></div>
-
 			</div>
 			<div class="sixteen wide column" style="">
 				<span style="font-size:11px;">${d.companySummary}</span>
