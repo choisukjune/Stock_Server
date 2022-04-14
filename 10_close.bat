68e8d31dada9f76d8f2cfe8586be850ac004e251
choisukjune <sukjune.choi@gmail.com>
2022-04-14 15:54:54 +0900

    Update 10_close.bat

--- 10_close.bat
+++ 10_close.bat
@@ -1,9 +1,13 @@
 ::@echo off
 
+SET yyyymmdd=%DATE:~0,4%%DATE:~5,2%%DATE:~8,2% 
+::SET yyyymmdd=%DATE:~10,4%%DATE:~4,2%%DATE:~7,2% 
+ECHO %yyyymmdd%
+
 ::for /L %%i in ( 0, 1, 1 ) do (
 ::@echo %DATE% %TIME% - %%i
-call "c:\Program Files\AutoHotkey\AutoHotkey.exe" 10_taradeValue_close.ahk %1
-call node 10_make__taradeValue_close.js %1
-call node 10_insert__taradeValue.js %1
+call "c:\Program Files\AutoHotkey\AutoHotkey.exe" 10_taradeValue_close.ahk %yyyymmdd%
+call node 10_make__taradeValue_close.js %yyyymmdd%
+call node 10_insert__taradeValue.js %yyyymmdd%
 ::)
 cmd /c
