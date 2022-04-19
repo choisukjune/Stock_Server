47c6c6db4d462dc652adf2903d3ed3c8a0c1e75e
choisukjune <sukjune.choi@gmail.com>
2022-04-19 15:59:07 +0900

    Update common.util.js

--- resource/js/common.util.js
+++ resource/js/common.util.js
@@ -391,7 +391,7 @@ window.COMPONENT.makeSearchList = function( arr, tDom, cb ){
 	for(;i<iLen;++i){
 		io = arr[ i ];
 		html = `
-		<div class="item" id="search_result_${io.displayedCode}" data-cd-value="${io.displayedCode}" style="cursor:pointer;">(${io.displayedCode}) - ${io.koreanName}</div>
+		<div class="item" id="search_result_${io.cd}" data-cd-value="${io.cd}" style="cursor:pointer;">(${io.cd}) - ${io.nm}</div>
 		`
 		var new_el = window.UTIL.Html.htmlToElement( html )
 		var target_el = window.document.getElementById( "search_result_" + io.displayedCode )
