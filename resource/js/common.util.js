ef2162d40030e3d85114cc27a631e75cf2197abe
choisukjune <sukjune.choi@gmail.com>
2022-04-22 19:09:04 +0900

    Update common.util.js

--- resource/js/common.util.js
+++ resource/js/common.util.js
@@ -3014,7 +3014,7 @@ window.COMPONENT.renderAgcDailyTreemap_pp = function( d, sort ){
 						}
 					}
 				},
-				levels: [{ color: ['blue', '#666' ,'red'],colorMappingBy: 'value',itemStyle: { gapWidth: 1 } }]
+				levels: [{ color: ['pink','red'],colorMappingBy: 'value',itemStyle: { gapWidth: 1 } }]
 			}
 		]
 	};
