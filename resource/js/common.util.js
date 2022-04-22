b6f1ec1e047b9020973c060bfd86c6e56668bd00
choisukjune <sukjune.choi@gmail.com>
2022-04-22 12:26:41 +0900

    Update common.util.js

--- resource/js/common.util.js
+++ resource/js/common.util.js
@@ -2878,7 +2878,7 @@ window.COMPONENT.renderAgcDailyTreemap_pp = function( d, sort ){
 			window.COMPONENT.renderAgcDailyTreemap_pp.cd = d.data.cd;
 			window.COMPONENT.getCandleChartByCd( d.data.cd, window.date.curBefore3month ,window.date.cur, function(d){
 				window.COMPONENT.renderAgcDailyTreemap_pp.data.a = d;
-				window.COMPONENT.getAcgDataByCd( window.charts.renderAgcDailyTreemap_pp.cd, window.date.curBefore3month, window.date.cur, function(d){
+				window.COMPONENT.getAcgDataByCd( window.COMPONENT.renderAgcDailyTreemap_pp.cd, window.date.curBefore3month, window.date.cur, function(d){
 					window.COMPONENT.renderAgcDailyTreemap_pp.data.b = d;
 					debugger;
 					window.COMPONENT.renderCandleChartByCdAndAcg()
